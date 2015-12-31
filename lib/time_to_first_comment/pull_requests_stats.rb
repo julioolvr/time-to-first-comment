454072ac4de9b39bd185c0621eb3ae714d987ab0
Julio Olivera <julio.olvr@gmail.com>
2015-12-31 13:52:43 -0300

    Date filters

--- lib/time_to_first_comment/pull_requests_stats.rb
+++ lib/time_to_first_comment/pull_requests_stats.rb
@@ -12,14 +12,20 @@ module TimeToFirstComment
     # Gets the time until the first comment for a given repo's pull requests.
     #
     # @param repo [String] A string supported by {Octokit::Client} like 'username/repo'
+    # @param options [Hash] Options for selecting the PRs to check.
+    # @option options [String] :from PRs created starting from this date. The format is YYYY-MM-DD.
+    # @option options [String] :to PRs created up to this date. The format is YYYY-MM-DD.
     # @return [Array<Array>] An array with tuples that include the pull request as the first parameter and the number of seconds
     #   until the first comment arrived as the second (`nil` if there are no comments).
     # @example Get the time to first comment for the PRs in the repo 'username/repo'
     #   client = TimeToFirstComment::PullRequestsStats.new(Octokit::Client.new)
-    #   client.time_to_first_comment('username/repo')
+    #   client.time_to_first_comment('username/repo', from: '2015-03-12', to: '2015-05-10')
     #   #=> [[<Sawyer::Resource ...>, 12000], [<Sawyer::Resource ...>, nil]]
-    def time_to_first_comment(repo)
-      pulls = client.search_issues("repo:#{repo} type:pr sort:updated")[:items]
+    def time_to_first_comment(repo, options = {})
+      repo_query = "repo:#{repo}"
+      date_range_query = date_query(:created, options[:from], options[:to])
+
+      pulls = client.search_issues("type:pr sort:updated #{repo_query} #{date_range_query}")[:items]
 
       pulls.map do |pull|
         [pull, seconds_until_first_comment(repo, pull)]
@@ -39,6 +45,27 @@ module TimeToFirstComment
       review_comments = client.review_comments(repo, pull.number)
       first_comment = (issue_comments + review_comments).sort_by(&:created_at).first
       first_comment.created_at - pull.created_at if first_comment
+    end
+
+    # Returns a query string for searching Github's API. It can be either for date created or updated, and with from and/or to
+    # dates.
+    #
+    # @param [Symbol] type Either :created or :updated
+    # @param [String] from Date from, the format is YYYY-MM-DD.
+    # @param [String] to Date to, the format is YYYY-MM-DD.
+    # @return [String] description A string for querying Github's API. An empty string if no date is provided.
+    def date_query(type, from, to)
+      if from && to
+        dates = "#{from}..#{to}"
+      elsif from
+        dates = ">=#{from}"
+      elsif to
+        dates = "<=#{to}"
+      else
+        return ''
+      end
+
+      "#{type}:#{dates}"
     end
   end
 end
